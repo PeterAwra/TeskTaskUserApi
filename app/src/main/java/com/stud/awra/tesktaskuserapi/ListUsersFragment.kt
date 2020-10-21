91aaddc817e19bf66f05b10cce3c510db85ac399
Peter Avramyshyn <peter.awra@gmail.com>
2020-10-22 02:36:54 +0300

    add transition

--- app/src/main/java/com/stud/awra/tesktaskuserapi/ListUsersFragment.kt
+++ app/src/main/java/com/stud/awra/tesktaskuserapi/ListUsersFragment.kt
@@ -3,10 +3,12 @@ package com.stud.awra.tesktaskuserapi
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.transition.MaterialElevationScale
 import com.stud.awra.tesktaskuserapi.model_data.User
 
 class ListUsersFragment : Fragment(R.layout.fragment_list_users) {
@@ -14,16 +16,25 @@ class ListUsersFragment : Fragment(R.layout.fragment_list_users) {
         override fun onClick(view: View, user: User) {
             val extras =
                 FragmentNavigatorExtras(
-                    view.findViewById<ImageView>(R.id.iv_icon_item_user) to user.email!!)
+                    view.findViewById<ImageView>(R.id.iv_icon_item_user) to getString(R.string.name_transition)
+                )
             findNavController().navigate(
                 ListUsersFragmentDirections.actionListUsersFragmentToUserFragment(user),
                 extras
             )
+            exitTransition = MaterialElevationScale(false).apply {
+                duration = resources.getInteger(R.integer.transition_duration).toLong()
+            }
+            reenterTransition = MaterialElevationScale(true).apply {
+                duration = resources.getInteger(R.integer.transition_duration).toLong()
+            }
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
         view.findViewById<RecyclerView>(R.id.rv_user_list).adapter =
             UsersAdapter(fakeUser(20), clickListener)
     }
--- app/src/main/java/com/stud/awra/tesktaskuserapi/UserFragment.kt
+++ app/src/main/java/com/stud/awra/tesktaskuserapi/UserFragment.kt
@@ -1,10 +1,13 @@
 package com.stud.awra.tesktaskuserapi
+
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.MaterialContainerTransform
 import com.stud.awra.tesktaskuserapi.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
@@ -19,5 +22,15 @@ class UserFragment : Fragment() {
         return FragmentUserBinding.inflate(inflater, container, false)
             .also { it.user = userArg.user }
             .root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = MaterialContainerTransform().apply {
+            drawingViewId = R.id.nav_host_container
+            duration = resources.getInteger(R.integer.transition_duration).toLong()
+            scrimColor = Color.TRANSPARENT
+            setAllContainerColors(Color.TRANSPARENT)
+        }
     }
 }
